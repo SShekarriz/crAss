c07b06e184e87fbd03d11418c66bea6ccd8d5867
Sharok Shekarriz <shahrokh.shekarriz@gmail.com>
2021-10-28 16:01:38 -0400

    starting the crAss proj

---
+++ crAss_pipe.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+####################################################################################
+DATE=`date '+%Y-%m-%d-%H:%M'`
+exec > >(tee -i CRASS_PIPE_${DATE}.log)
+exec 2>&1
+####################################################################################
+
+
+Metagenome=$PWD/Assembled_metagenomes
+comple_crAss=$PWD/crAss_genomes/complete_genomes.fa
+blastOut=$PWD/crAss_in_metagenome
+
+mkdir -p $blastOut
+
+echo "############################"
+echo "Finding crAss in metagenomes"
+echo "############################"
+
+cd $Metagenome
+ext="*_contigs.fasta"
+for fna in $ext
+do
+        echo $fna
+        fnaid=${fna%$ext}
+        echo $fnaid
+	
+	echo "######### create blast dbs ################"	
+	out=${fnaid}_db.ndb
+        if [ -f $out ]; then
+                echo "'$out' exists."
+        else
+                echo "'$out' is not found."
+		makeblastdb -in $fna -dbtype nucl -out ${fnaid}_db
+	fi
+
+	echo "######### search crAss in metagenomes #########"
+	out=$blastOut/${fnaid}.blastout
+        if [ -f $out ]; then
+                echo "'$out' exists."
+        else
+                echo "'$out' is not found."
+                blastn -db ${fnaid}_db -query $comple_crAss -out $out -outfmt "6 std qcovhsp" -num_threads 25
+        fi	
+
+
+
+done
+
+
+echo "################### DONE ####################################"
+date -u
+echo "#############################################################"
